0724453a1746333067dcef4571f3a117a14f97b2
Raphael Martins <Raphiusk.Rio@gmail.com>
2023-07-02 16:25:12 -0300

    Separando responsabilidades o arquivo server.js erguerá o server

---
+++ server.js
@@ -0,0 +1,6 @@
+import app from './src/app.js';
+
+const PORT = 5500;
+
+
+app.listen(PORT, ()=> {console.log(`Server ON.\nPort:${PORT}`)});
--- src/app.js
+++ src/app.js
@@ -1,10 +1,27 @@
 import express from 'express';
 
 const app = express();
-const port = 5500;
+
+//mock
+let filmes90 = [
+    {id: 1, titulo: "A Lista de Schindler", duracao: "3h 15min", minutos: 195},
+    {id: 2, titulo: "Um Sonho de Liberdade", duracao: "2h 22min", minutos: 142},
+    {id: 3, titulo: "O Rei Leão (animação da Disney)", duracao: "1h 27min", minutos: 87},
+    {id: 4, titulo: "Forrest Gump, O Contador de Histórias", duracao: "2h 22min", minutos: 142},
+    {id: 5, titulo: "A Vida é Bela", duracao: "1h 56min", minutos: 116},
+    {id: 6, titulo: "O Resgate do Soldado Ryan", duracao: "2h 50min", minutos: 170},
+    {id: 7, titulo: "10 Coisas Que Odeio em Você", duracao: "1h 37min", minutos: 97},
+    {id: 8, titulo: "Jurassic Park", duracao: "2h 7min", minutos: 127},
+    {id: 9, titulo: "O Silêncio dos Inocentes", duracao: "1h 58min", minutos: 118},
+    {id: 10, titulo: "À Espera de um Milagre", duracao: "3h 9min", minutos: 189}
+]
 
 app.get('/', (req, res)=>{
     res.send("Hello world!");
 })
 
-app.listen(port, ()=> {console.log(`Server ON.\nPort:${port}`)});
+app.get('/filmes/90', (req, res) => {
+    res.status(200).send(filmes90)
+})
+
+export default app;
