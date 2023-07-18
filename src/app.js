e7734769e9e7ea272b8ea5059705c98041bb7c5d
Raphael Martins <Raphiusk.Rio@gmail.com>
2023-07-18 16:47:59 -0300

    metodo index implementado

--- src/app.js
+++ src/app.js
@@ -1,20 +1,11 @@
 import express from 'express';
 import conexao from './app/database/conexao.js';
-
+import Filme90Controller from './app/controllers/Filme90Controller.js';
 const app = express();
 
 app.use(express.json())
 
-app.get('/filmes/90', (req, res) => {
-    const sql = "SELECT * FROM tb_filmes90;"
-    conexao.query(sql, (error, result) => {
-        if(error){
-            res.status(404).json( {'Error': error})
-        } else {
-            res.status(200).json(result);
-        }
-    })
-})
+app.get('/filmes/90', Filme90Controller.index);
 
 //ROTAS
 app.get('/filmes/90/:id', (req, res) => {
--- src/app/controllers/Filme90Controller.js
+++ src/app/controllers/Filme90Controller.js
@@ -1,7 +1,16 @@
 class Filme90Controller {
 
     //lista tudo
-    index() {}
+    index(req, res) {
+        const sql = "SELECT * FROM tb_filmes90;"
+        conexao.query(sql, (error, result) => {
+            if(error){
+                res.status(404).json( {'Error': error})
+            } else {
+                res.status(200).json(result);
+            }
+        })
+    }
     
     //lista por ID
     show() {} 
