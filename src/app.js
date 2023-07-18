34434b5c0733e644ba0dd6fdb8eed857b37d1d47
Raphael Martins <Raphiusk.Rio@gmail.com>
2023-07-18 16:12:32 -0300

    Organizando pastas MVC

--- src/app.js
+++ src/app.js
@@ -5,12 +5,10 @@ const app = express();
 
 app.use(express.json())
 
-//Lê os objetos do array
 app.get('/filmes/90', (req, res) => {
     const sql = "SELECT * FROM tb_filmes90;"
     conexao.query(sql, (error, result) => {
         if(error){
-            console.log(error);
             res.status(404).json( {'Error': error})
         } else {
             res.status(200).json(result);
@@ -18,14 +16,13 @@ app.get('/filmes/90', (req, res) => {
     })
 })
 
-//Lê objeto no array pelo seu ID
+//ROTAS
 app.get('/filmes/90/:id', (req, res) => {
     const id = req.params.id;
     const sql = "SELECT * FROM tb_filmes90 WHERE id=?;"
     conexao.query(sql, id, (error, result) => {
         const row = result[0];
         if(error){
-            console.log(error);
             res.status(404).json( {'Error': error})
         } else {
             res.status(200).json(row);
@@ -33,28 +30,24 @@ app.get('/filmes/90/:id', (req, res) => {
     })
 })
 
-//Cria um objeto no array
 app.post('/filmes/90', (req, res) => {
     const filme90 = req.body;
     const sql = "INSERT INTO tb_filmes90 SET ?;"
     conexao.query(sql, filme90, (error, result) => {
         if(error){
-            console.log(error);
-            res.status(400).json( {'Error': error})
+            res.status(404).json( {'Error': error})
         } else {
             res.status(201).json(result);
         }
     })
 })
 
-//Exclui objeto do array pelo ID informado
 app.delete('/filmes/90/:id', (req, res) => {
     const id = req.params.id;
     const sql = "DELETE * FROM tb_filmes90 WHERE id=?;"
     conexao.query(sql, id, (error, result) => {
         const row = result[0];
         if(error){
-            console.log(error);
             res.status(404).json( {'Error': error})
         } else {
             res.status(200).json(row);
@@ -62,15 +55,13 @@ app.delete('/filmes/90/:id', (req, res) => {
     })
 })
 
-//Atualiza objeto no array pelo seu ID
 app.put('/filmes/90/:id', (req, res) => {
     const id = req.params.id;
     const filme90 = req.body;
     const sql = "UPDATE tb_filmes90 SET ? WHERE id=?;"
     conexao.query(sql, [filme90, id], (error, result) => {
         if(error){
-            console.log(error);
-            res.status(400).json( {'Error': error})
+            res.status(404).json( {'Error': error})
         } else {
             res.status(200).json(result);
         }
---
+++ src/app/controllers/Filme90Controller.js
@@ -0,0 +1,12 @@
+class Filme90Controller {
+
+    index() {} //lista tudo
+    show() {} //lista por ID
+    store() {} //criar dados
+    update() {} //atualiza dados
+    delete() {} //exclui dados
+
+}
+
+//padrão Singleton
+export default new Filme90Controller();
